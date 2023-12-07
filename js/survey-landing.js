2a19493003b542eb168e8e018f12974a5c5cdcc2
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-12-07 15:52:57 +0200

    Add files via upload

--- js/survey-landing.js
+++ js/survey-landing.js
@@ -60,34 +60,237 @@ $(document).ready(function () {
 
     // steps
     // NEXT
-    $('.sectstep1 .nextst').click(function () {
-        $('.sectstep').addClass('hide');
-        $('.sectstep2').removeClass('hide');
+    // $('.sectstep1 .nextst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep2').removeClass('hide');
 
-        $('.actualstep').text(2);
-        $('.stepsboxes').removeClass(function (index, className) {
-            return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
-        });
-        $('.stepsboxes').addClass('stepsboxes-2');
-    })
+    //     $('.actualstep').text(2);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-2');
+    // })
 
-   
+    // $('.sectstep2 .nextst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep3').removeClass('hide');
+
+    //     $('.actualstep').text(3);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-3');
+    // })
+
+    // $('.sectstep3 .nextst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep4').removeClass('hide');
+
+    //     $('.actualstep').text(4);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-4');
+    // })
+
+    // $('.sectstep4 .nextst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep5').removeClass('hide');
+
+    //     $('.actualstep').text(5);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-5');
+    // })
+
+    // $('.sectstep5 .nextst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep6').removeClass('hide');
+
+    //     $('.actualstep').text(6);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-6');
+    // })
+
+
+
+
+
+    // function goToStep(step) {
+    //     $('.sectstep').addClass('hide');
+
+    //     var nextStepClass = '.sectstep' + step;
+    //     $(nextStepClass).removeClass('hide');
+
+    //     $('.actualstep').text(step);
+
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-' + step);
+    // }
+
+    // $('.sectstep1 .nextst').click(function () {
+    //     goToStep(2);
+    // });
+
+    // $('.sectstep2 .nextst').click(function () {
+    //     goToStep(3);
+    // });
+
+    // $('.sectstep3 .nextst').click(function () {
+    //     goToStep(4);
+    // });
+
+    // $('.sectstep4 .nextst').click(function () {
+    //     goToStep(5);
+    // });
+
+    // $('.sectstep5 .nextst').click(function () {
+    //     goToStep(6);
+    // });
+
+
+
+
 
 
     // PREV
-    $('.sectstep2 .prevst').click(function () {
-        $('.sectstep').addClass('hide');
-        $('.sectstep1').removeClass('hide');
+    // $('.sectstep2 .prevst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep1').removeClass('hide');
 
-        $('.actualstep').text(1);
+    //     $('.actualstep').text(1);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-1');
+
+    // });
+    // $('.sectstep3 .prevst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep2').removeClass('hide');
+
+    //     $('.actualstep').text(2);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-2');
+
+    // });
+
+    // $('.sectstep4 .prevst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep3').removeClass('hide');
+
+    //     $('.actualstep').text(3);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-3');
+
+    // });
+
+    // $('.sectstep5 .prevst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep4').removeClass('hide');
+
+    //     $('.actualstep').text(4);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-4');
+
+    // });
+
+    // $('.sectstep6 .prevst').click(function () {
+    //     $('.sectstep').addClass('hide');
+    //     $('.sectstep5').removeClass('hide');
+
+    //     $('.actualstep').text(5);
+    //     $('.stepsboxes').removeClass(function (index, className) {
+    //         return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
+    //     });
+    //     $('.stepsboxes').addClass('stepsboxes-5');
+
+    // });
+
+
+
+
+
+
+    function goToStep(step) {
+        $('.sectstep').addClass('hide');
+
+        var nextStepClass = '.sectstep' + step;
+        $(nextStepClass).removeClass('hide');
+
+        $('.actualstep').text(step);
+
         $('.stepsboxes').removeClass(function (index, className) {
             return (className.match(/(^|\s)stepsboxes-\S+/g) || []).join(' ');
         });
-        $('.stepsboxes').addClass('stepsboxes-1');
+        $('.stepsboxes').addClass('stepsboxes-' + step);
+    }
 
+    // next
+    $('.sectstep1 .nextst').click(function () {
+        goToStep(2);
+    });
+
+    $('.sectstep2 .nextst').click(function () {
+        goToStep(3);
+    });
+
+    $('.sectstep3 .nextst').click(function () {
+        goToStep(4);
+    });
+
+    $('.sectstep4 .nextst').click(function () {
+        goToStep(5);
+    });
+
+    $('.sectstep5 .nextst').click(function () {
+        goToStep(6);
+    });
+
+    //prev
+    $('.sectstep2 .prevst').click(function () {
+        goToStep(1);
+    });
+
+    $('.sectstep3 .prevst').click(function () {
+        goToStep(2);
+    });
+
+    $('.sectstep4 .prevst').click(function () {
+        goToStep(3);
+    });
+
+    $('.sectstep5 .prevst').click(function () {
+        goToStep(4);
+    });
+
+    $('.sectstep6 .prevst').click(function () {
+        goToStep(5);
     });
 
 
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
 })
